9b1afe9712f54128592a59d0d60be485c8ead155
Almir Sarajčić <TB_Almir@hotmail.com>
2014-08-25 12:01:05 +0200

    Solve "book_titles"

---
+++ 08_book_titles/book.rb
@@ -0,0 +1,23 @@
+class Book
+  attr_reader :title
+
+  def title=(new_title)
+    words = new_title.split
+
+    words = [words[0].capitalize] + words[1..-1].map do |word|
+      if little_words.include? word
+        word
+      else
+        word.capitalize
+      end
+    end
+
+    @title = words.join(' ')
+  end
+
+  private
+
+  def little_words
+    @little_words ||= %w{ a an and the in of }
+  end
+end
